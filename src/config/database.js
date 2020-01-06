4bc5273f4119563d273ee9491bde7f1848ed99dc
David Lima <DavidLima@alu.ufc.br>
2020-01-06 06:01:58 -0300

    Configured sequelize and postgresqls; Created schools migration

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: '',
+  database: 'redacao-up',
+  define: {
+    timestamps: true,
+    underscored: true,
+  },
+};
---
+++ src/database/migrations/20200106084110-create-schools.js
@@ -0,0 +1,41 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('schools', {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    state: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    city: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    avatar: Sequelize.STRING,
+    created_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    updated_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+  }),
+
+  down: (queryInterface) => queryInterface.dropTable('schools'),
+};
