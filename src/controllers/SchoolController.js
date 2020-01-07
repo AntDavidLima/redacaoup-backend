1017ae6cbb86d02069d2fd4b7134a35d924daf0e
David Lima <DavidLima@alu.ufc.br>
2020-01-07 07:36:24 -0300

    Created school controller

--- src/controllers/SchoolController.js
+++ src/controllers/SchoolController.js
@@ -6,10 +6,60 @@ module.exports = {
       name, state, city, email, password, avatar,
     } = req.body;
 
-    const school = await School.create({
-      name, state, city, email, password, avatar,
+    const [school, created] = await School.findOrCreate({
+      where: { email },
+      defaults: {
+        name, state, city, email, password, avatar,
+      },
     });
 
+    if (!created) return res.status(409).json({ error: 'This account alredy exists' });
+
+    return res.json(school);
+  },
+
+  async index(req, res) {
+    const schools = await School.findAll();
+    return res.json(schools);
+  },
+
+  async show(req, res) {
+    const { school_id } = req.params;
+
+    const school = await School.findByPk(school_id);
+
+    if (school === null) return res.status(406).json({ error: 'School not found!' });
+
+    return res.json(school);
+  },
+
+  async destroy(req, res) {
+    const { school_id } = req.params;
+
+    const school = await School.findByPk(school_id);
+
+    if (school === null) return res.status(406).json({ error: 'School not found!' });
+
+    await School.destroy({ where: { id: school_id } });
+
+    return res.send();
+  },
+
+  async update(req, res) {
+    const { school_id } = req.params;
+    const {
+      name, state, city, email, password, avatar,
+    } = req.body;
+
+    const [wasUpdate] = await School.update({
+      name, state, city, email, password, avatar,
+    }, {
+      where: { id: school_id },
+    });
+
+    if (wasUpdate === 0) return res.status(500).json({ error: 'Could not update this school!' });
+
+    const school = await School.findByPk(school_id);
     return res.json(school);
   },
 };
--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,13 @@ const StudentController = require('./controllers/StudentController');
 const ThemeController = require('./controllers/ThemeController');
 const EssayController = require('./controllers/EssayController');
 
+// School routes
 routes.post('/schools', SchoolController.store);
+routes.get('/schools', SchoolController.index);
+routes.get('/schools/:school_id', SchoolController.show);
+routes.delete('/schools/:school_id', SchoolController.destroy);
+routes.put('/schools/:school_id', SchoolController.update);
+
 routes.post('/schools/:school_id/students', StudentController.store);
 routes.post('/schools/:school_id/themes', ThemeController.store);
 routes.post('/students/:student_id/themes/:theme_id/essays', EssayController.store);
